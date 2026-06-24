c16476c95deef6109395f08cea190cef58c07cfd
krishpdgl-netizen <krishpdgl@gmail.com>
2026-06-24 11:12:00 +0530

    Create mailer.py

---
+++ mailer.py
@@ -0,0 +1,36 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+SMTP_EMAIL = os.getenv("SMTP_EMAIL")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
+
+DASHBOARD_URL = os.getenv("DASHBOARD_URL")
+
+
+def send_sales_reminder(name, email):
+
+    body = f"""
+Hi {name},
+
+Please update your weekly sales projections.
+
+Dashboard:
+{DASHBOARD_URL}
+
+Kindly complete it before EOD Monday.
+
+Regards,
+Sales Team
+"""
+
+    msg = MIMEText(body)
+
+    msg["Subject"] = "Weekly Sales Projection Reminder"
+    msg["From"] = SMTP_EMAIL
+    msg["To"] = email
+
+    with smtplib.SMTP("smtp.gmail.com",587) as server:
+        server.starttls()
+        server.login(SMTP_EMAIL, SMTP_PASSWORD)
+        server.send_message(msg)
