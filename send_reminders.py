2fca64e3d5ce401ad11b00aaf7d29e2e7194d1d3
krishpdgl-netizen <krishpdgl@gmail.com>
2026-06-24 11:12:51 +0530

    Create send_reminders.py

---
+++ send_reminders.py
@@ -0,0 +1,41 @@
+from sqlalchemy import create_engine,text
+from mailer import send_sales_reminder
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode":"require"}
+)
+
+with engine.connect() as conn:
+
+    users = conn.execute(
+        text("""
+        SELECT
+            full_name,
+            email
+        FROM users
+        WHERE
+            receive_sales_reminder=TRUE
+        AND
+            role='employee'
+        ORDER BY full_name
+        """)
+    ).fetchall()
+
+for row in users:
+
+    try:
+
+        send_sales_reminder(
+            row.full_name,
+            row.email
+        )
+
+        print("Sent to",row.full_name)
+
+    except Exception as e:
+
+        print(e)
