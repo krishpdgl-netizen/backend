52a4c854e39b5589507a9bf8e5609ef6340e205c
krishpdgl-netizen <krishpdgl@gmail.com>
2026-06-24 11:34:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ from excel_helper import (
 )
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from send_reminders import send_all_reminders
+
+
 
 app = FastAPI()
 app.add_middleware(
@@ -2151,3 +2154,11 @@ def get_manager_remarks(manager_id: int):
         ).fetchall()
 
     return [dict(r._mapping) for r in rows]
+@app.get("/test-mail")
+def test_mail():
+
+    send_all_reminders()
+
+    return {
+        "message": "mail sent"
+    }
