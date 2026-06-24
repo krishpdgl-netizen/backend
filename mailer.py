b1f03aa6994729a6592a76b34609768fe75cca7e
krishpdgl-netizen <krishpdgl@gmail.com>
2026-06-24 11:47:12 +0530

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,14 +1,16 @@
 import os
 import smtplib
 from email.mime.text import MIMEText
+from datetime import datetime
 
 SMTP_EMAIL = os.getenv("SMTP_EMAIL")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
-
 DASHBOARD_URL = os.getenv("DASHBOARD_URL")
 
 
 def send_sales_reminder(name, email):
+
+    week_no = datetime.now().isocalendar()[1]
 
     body = f"""
 Hi {name},
@@ -26,15 +28,11 @@ Sales Team
 
     msg = MIMEText(body)
 
-from datetime import datetime
+    msg["Subject"] = f"Week {week_no} Sales Projection Reminder"
+    msg["From"] = SMTP_EMAIL
+    msg["To"] = email
 
-week_no = datetime.now().isocalendar()[1]
-
-msg["Subject"] = f"Week {week_no} Sales Projection Reminder"
-msg["From"] = SMTP_EMAIL
-msg["To"] = email
-
-    with smtplib.SMTP("smtp.gmail.com",587) as server:
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
         server.starttls()
         server.login(SMTP_EMAIL, SMTP_PASSWORD)
         server.send_message(msg)
